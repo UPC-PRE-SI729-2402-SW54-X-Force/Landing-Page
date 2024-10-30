274850185d00b9ba19f597084ca28ba53d07493b
Al3xiel <gabriel280104casas@gmail.com>
2024-10-30 15:35:23 -0500

    feat(i18n): added translation to spanish.

--- src/app/premium-plans/premium-plans.component.ts
+++ src/app/premium-plans/premium-plans.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-premium-plans',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './premium-plans.component.html',
   styleUrls: ['./premium-plans.component.css']
 })
--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-team',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.css']
 })
